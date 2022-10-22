3409152c5fa9c8c333ffc6cb857bca0b3fa681fd
akashproject <akashdutta.scriptcrown@gmail.com>
2022-10-22 12:51:34 +0530

    Order Success Mail Template

--- administrator/resources/views/emails/order.blade.php
+++ administrator/resources/views/emails/order.blade.php
@@ -2,39 +2,65 @@
     <div style="width: 100%;padding-bottom: 20px;border-bottom: 1px solid #ccc;">
         <img src="http://bikriworld.com/assets/images/logo.png" style="width: 20%;">
     </div>
-    <h3> Dear Seller, </h3><h3>
-    <p style="margin: 0;"> Woohoo! We Love you already. Thank you for choosing to BikriWorld for your devices. </p>
-    <p style="margin: 20px 0;"> We created a Service Number: 11222 for you, and the ability to track your Devices Status. </p> 
-    <table style="border: 2px solid;margin: 30px 0;">
-        <thead>
-            <tr style="background: #00986b;color: #fff;">
-                <th colspan="2" style="padding: 10px 20px;"> Order Details </th>
-            </tr>
-        </thead>
-        <tbody style="">
-            <tr>
-                <td style="padding: 10px 20px 10px;font-size: 20px;"> Service No </td>
-                <td style="padding: 10px 20px 10px;font-size: 20px;"> 11222 </td>
-            </tr>
-            <tr>
-                <td style="padding: 10px 20px 10px;font-size: 20px;"> Pickup address </td>
-                <td style="padding: 10px 20px 10px;font-size: 20px;"> 2555  </td>
-            </tr>
-            <tr>
-                <td style="padding: 10px 20px 10px;font-size: 20px;"> Payment Mode </td>
-                <td style="padding: 10px 20px 10px;font-size: 20px;"> ddd </td>
-            </tr>
-            <tr>
-                <td style="padding: 10px 20px 10px;font-size: 20px;"> Amount </td>
-                <td style="padding: 10px 20px 10px;font-size: 20px;"> Rs. 100/- </td>
-            </tr>
-        </tbody>
-    </table>
-    <p>        
-        If you have queries, suggestions or just a good discussions. Drop me a message at the support@BikriWorld.in . We hope to see you again!
+    <h3> Dear Seller, </h3>
+    <p style="margin: 0;"> Thank you for choosing to BikriWorld. </p>
+    <p style="margin: 20px 0;"> We have recived your device successfully regards of Service No : 11222.  </p> 
+    <div style="text-align: center;">
+        <table style="border: 2px solid;margin: 30px 0;width: 100%;">
+            <thead>
+                <tr style="background: #00986b;color: #fff;">
+                    <th colspan="2" style="padding: 10px 20px;"> BikriWorld Invoice </th>
+                </tr>
+            </thead>
+            <tbody >
+               
+                <tr>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;background: #00855db3;color: #fff;"> Device Name </td>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;width: 50%;border-bottom: 1px solid #82c3b0;"> HP 300 Series </td>
+                </tr>
+                <tr>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;background: #00855db3;color: #fff;"> Device Configuration </td>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;width: 50%;border-bottom: 1px solid #82c3b0;"> i5 Processer | 8GB Ram | 500GB Hard Hisk </td>
+                </tr>
+                <tr style="text-align: center;">
+                    <td colspan="2"style="padding: 10px 20px 10px;font-size: 20px;background: #00855db3;color: #fff;"> Order Detail </td>
+                </tr>
+                <tr>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;background: #00855db3;color: #fff;"> Service No </td>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;width: 50%;border-bottom: 1px solid #82c3b0;"> 11222 </td>
+                </tr>
+                <tr>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;background: #00855db3;color: #fff;"> Pickup address </td>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;width: 50%;border-bottom: 1px solid #82c3b0;"> 2555  </td>
+                </tr>
+                <tr>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;background: #00855db3;color: #fff;"> Payment Mode </td>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;width: 50%;border-bottom: 1px solid #82c3b0;"> ddd </td>
+                </tr>
+                <tr>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;background: #00855db3;color: #fff;"> Recived Date </td>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;width: 50%;border-bottom: 1px solid #82c3b0;"> ddd </td>
+                </tr>
+                <tr>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;background: #00855db3;color: #fff;"> Purchasing Amount </td>
+                    <td style="padding: 10px 20px 10px;font-size: 20px;width: 50%;border-bottom: 1px solid #82c3b0;"> Rs. 100/- </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <p style="margin: 0;">        
+        If you have queries, suggestions or just a good discussions. Drop me a message at the support@bikriworld.in . We hope to see you again!
     </p>
-    <p> 
-        Team BikriWorld!
+    <p > 
+        Thanks & Regards <br>
+        <strong>Team BikriWorld!</strong>
     </p>
-
-</h3></div></body>
+    <div style="background-color: #00986b;padding: 25px;text-align: center;">
+        <ul style="margin: 0;padding: 0;">
+            <li style="display: inline;"> <a style="font-size: 18px;padding-right: 20px;color: #fff;text-decoration: none;" href="" > Privacy Policy </a></li>
+            <li style="display: inline;"> <a style="font-size: 18px;padding-right: 20px;color: #fff;text-decoration: none;" href="" > Term & Conditions </a></li>
+            <li style="display: inline;"> <a style="font-size: 18px;padding-right: 20px;color: #fff;text-decoration: none;" href="" > Contact Us </a></li>
+        </ul>
+        <p style="font-size: 8px;padding-right: 20px;color: #fff;margin-top: 51px;"> All Copyright 2022 @ BikriWorld. All rights reserved </p>
+    </div>
+</body>
